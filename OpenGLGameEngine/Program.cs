1d1ca4893a39916e85a33a6c229f3ed3c7123d21
merfman <garyarmstrong100@gmail.com>
2025-02-23 18:47:19 -0500

    Add project files.

---
+++ OpenGLGameEngine/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            ClientSize = new Vector2i(1440, 1080),
+            Title = "LearnOpenTK - Multiple lights",
+            // This is needed to run on macos
+            Flags = ContextFlags.ForwardCompatible,
+        };
+
+        using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+        {
+            Console.WriteLine("Program Loaded");
+            window.Run();
+        }
+        Console.WriteLine("Program Closed");
+    }
+}
