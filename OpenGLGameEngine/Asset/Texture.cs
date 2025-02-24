401a5f583512cd488c60e912e5b91b33c2d1c411
merfman <garyarmstrong100@gmail.com>
2025-02-23 21:00:39 -0500

    filled out more of Texture

--- OpenGLGameEngine/Asset/Texture.cs
+++ OpenGLGameEngine/Asset/Texture.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace OpenGLGameEngine.Asset;
 public class Texture : Asset
 {
+    public int Handle; // OpenGL 2d texture handle
+    public int Width;
+    public int Height;
+    public PixelInternalFormat Format;
+    public TextureWrapMode WrapMode;
     public override void LoadFromFile(string path)
     {
         throw new NotImplementedException();
