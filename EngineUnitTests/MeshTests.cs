0f5b46d210490b4800cc3e422049f0964c377806
merfman <garyarmstrong100@gmail.com>
2025-02-23 22:22:11 -0500

    added unit testing project to solution

---
+++ EngineUnitTests/MeshTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenGLGameEngine.Asset;
+
+
+namespace EngineUnitTests
+{
+    [TestClass]
+    public class MeshTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Assert.ThrowsException<Exception>(() => OBJParser.Parse(""));
+            //somthing like this
+        }
+    }
+}
---
+++ EngineUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EngineUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
