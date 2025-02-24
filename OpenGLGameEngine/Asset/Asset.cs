0d1f50c894f7333b2e982dc35ab82af0aa8dbc09
merfman <garyarmstrong100@gmail.com>
2025-02-23 20:54:45 -0500

    added Asset class and texture class deriving from it

---
+++ OpenGLGameEngine/Asset/Asset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGLGameEngine.Asset;
+public abstract class Asset : BaseObect
+{
+    public string Path { get; private set; }
+    public bool IsLoaded { get; private set; }
+
+    public abstract void LoadFromFile(string path);
+}
---
+++ OpenGLGameEngine/Asset/Texture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace OpenGLGameEngine.Asset;
+public class Texture : Asset
+{
+    public override void LoadFromFile(string path)
+    {
+        throw new NotImplementedException();
+    }
+}
