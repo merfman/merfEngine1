b8830da55a1f19cec6587e32b8e7b083e6ed7f3f
merfman <garyarmstrong100@gmail.com>
2025-02-26 13:21:28 -0500

    Addded Renderer Class and added to Window

---
+++ OpenGLGameEngine/Render.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Mathematics;
+
+namespace OpenGLGameEngine;
+/// <summary>
+/// Handles all the rendering. Not static so it can support multiple windows. 
+/// </summary>
+internal class Render
+{
+    /// <summary>
+    /// List of GameObjects that are rendered
+    /// </summary>
+    List<GameObject> RenderList = new List<GameObject>();
+
+
+
+}
--- OpenGLGameEngine/Window.cs
+++ OpenGLGameEngine/Window.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace OpenGLGameEngine;
 internal class Window : GameWindow
 {
+    private Render renderer = new Render();
+
     public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
     {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+    }
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+    }
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
     }
 }
